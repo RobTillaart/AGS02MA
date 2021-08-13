22f51062adcf10974ea4f9fbfabe623082e9d519
rob tillaart <rob.tillaart@gmail.com>
2021-08-13 09:04:31 +0200

    add I2CResetSpeed()

--- AGS02MA.cpp
+++ AGS02MA.cpp
@@ -60,7 +60,7 @@ bool AGS02MA::isConnected()
 #endif
   _wire->beginTransmission(_address);
   bool rv =  ( _wire->endTransmission() == 0);
-  _wire->setClock(400000);
+  _wire->setClock(_I2CReseSpeed);
   return rv;
 }
 
@@ -202,14 +202,14 @@ bool AGS02MA::_readRegister(uint8_t reg)
   if (_wire->requestFrom(_address, (uint8_t)5) != 5)
   {
     _error = AGS02MA_ERROR;
-    _wire->setClock(400000);
+    _wire->setClock(_I2CReseSpeed);
     return false;
   }
   for (int i = 0; i < 5; i++)
   {
     _buffer[i] = _wire->read();
   }
-  _wire->setClock(400000);
+  _wire->setClock(_I2CReseSpeed);
   return true;
 }
 
@@ -228,7 +228,7 @@ bool AGS02MA::_writeRegister(uint8_t reg)
     _wire->write(_buffer[i]);
   }
   _error = _wire->endTransmission();
-  _wire->setClock(400000);
+  _wire->setClock(_I2CReseSpeed);
   return (_error == 0);
 }
 
